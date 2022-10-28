3fc5cb6ed0fe066a140220e96df416ba0c7b611d
Manar <manar.qasem732016@gmail.com>
2022-10-28 17:28:01 +0300

    create comment class

--- src/modules/comments.js
+++ src/modules/comments.js
@@ -5,7 +5,15 @@ class comment {
         this.comment = comment ;
     }
 
-    displayComment = async () => {
-        const url = '';
-    }
+     sendData = async (data) => {
+        const postUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/h8BP3W6m1O3XvqYPoI4N/comments';
+        const parameters = {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(data),
+        };
+        await fetch(postUrl, parameters);
+      };
 }
